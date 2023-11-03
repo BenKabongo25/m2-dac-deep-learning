fba971e9633b96266dd039e8681de78d85512cc8
Ben Kabongo <kabongo.ben025@gmail.com>
2023-11-03 14:04:57 +0100

    TP05 : Beam Search

--- TPs/TP05/src/generate.py
+++ TPs/TP05/src/generate.py
@@ -1,49 +1,141 @@
-from textloader import  string2code, id2lettre
+# AMAL Master DAC
+# Novembre 2023
+
+# Ben Kabongo
+# M2 MVA
+
 import math
 import torch
 
-#  TODO:  Ce fichier contient les différentes fonction de génération
-
-def generate(rnn, emb, decoder, eos, start="", maxlen=200):
-    """  Fonction de génération (l'embedding et le decodeur être des fonctions du rnn). Initialise le réseau avec start (ou à 0 si start est vide) et génère une séquence de longueur maximale 200 ou qui s'arrête quand eos est généré.
-        * rnn : le réseau
-        * emb : la couche d'embedding
-        * decoder : le décodeur
-        * eos : ID du token end of sequence
-        * start : début de la phrase
-        * maxlen : longueur maximale
-    """
-
-    #  TODO:  Implémentez la génération à partir du RNN, et d'une fonction decoder qui renvoie les logits (logarithme de probabilité à une constante près, i.e. ce qui vient avant le softmax) des différentes sorties possibles
+from textloader import  *
 
 
-def generate_beam(rnn, emb, decoder, eos, k, start="", maxlen=200):
-    """
-        Génere une séquence en beam-search : à chaque itération, on explore pour chaque candidat les k symboles les plus probables; puis seuls les k meilleurs candidats de l'ensemble des séquences générées sont conservés (au sens de la vraisemblance) pour l'itération suivante.
-        * rnn : le réseau
-        * emb : la couche d'embedding
-        * decoder : le décodeur
-        * eos : ID du token end of sequence
-        * k : le paramètre du beam-search
-        * start : début de la phrase
-        * maxlen : longueur maximale
-    """
-    #  TODO:  Implémentez le beam Search
+def generate(model, start='', max_length=200):
+    prediction = torch.tensor([lettre2id[start]])
+    codes = []
+    with torch.no_grad():
+        h = torch.zeros(1, model.hidden_dim)
+        for _ in range(max_length):
+            h, output = model(prediction, h)
+            prediction = torch.softmax(output.squeeze(), dim=1).argmax(1).squeeze()
+            code = prediction.item()
+            codes.append(code)
+            if code == EOS_IX:
+                break
+    text = code2string(codes)
+    return text
 
 
-# p_nucleus
-def p_nucleus(decoder, alpha: float):
-    """Renvoie une fonction qui calcule la distribution de probabilité sur les sorties
+class BeamTree:
+    """ Noeud pour l'arbre de recherche Beam Search """
+    def __init__(self, code, prob):
+        self.code = code            # code du noeud
+        self.prob = prob            # log proba du noeud
+        self.cum_child_prob = 0     # meilleure log proba cumulée des noeuds fils
+        self.children = []          # noeuds fils
+        self.best_child = None      # meilleur noeud fils
+        self.predicted = False      # la prédiction a été faite pour ce noeud
+        self.index = 0              # index de la prédiction dans la séquence
 
-    Args:
-        * decoder: renvoie les logits étant donné l'état du RNN
-        * alpha (float): masse de probabilité à couvrir
-    """
-    def compute(h):
-        """Calcule la distribution de probabilité sur les sorties
 
-        Args:
-           * h (torch.Tensor): L'état à décoder
-        """
-        #  TODO:  Implémentez le Nucleus sampling ici (pour un état s)
-    return compute
+def find_best(tree):
+    """ Recherche du chemin qui maximise la log probabilité"""
+    if tree.children == []:
+        return tree
+    best_child = None
+    best_cum_child_prob = 0
+    for child in tree.children:
+        child = find_best(child)
+        cum_child_prob = child.prob + child.cum_child_prob
+        if cum_child_prob > best_cum_child_prob:
+            best_cum_child_prob = cum_child_prob
+            best_child = child
+    tree.best_child = best_child
+    tree.cum_child_prob = best_cum_child_prob
+    return tree
+
+
+def _generate_node(model, K, node, max_length):
+    """ Génération des K meilleures prédictions pour un noeud Beam Search """
+
+    # on arrête si le noeud a un code de fin de phrase ou si l'index max est atteint
+    if node.predicted or node.code == EOS_IX or node.index == max_length:
+        node.predicted = True
+        return node
+
+    # on prédit et on retient les K meilleures prédictions
+    prediction = torch.tensor([node.code])
+    with torch.no_grad():
+        h = torch.zeros(1, model.hidden_dim)
+        h, output = model(prediction, h)
+        log_probs, indices = torch.topk(output, K, dim=1)
+        children = []
+        for code, prob in zip(indices, log_probs):
+            child = BeamTree(code, prob)
+            child.index = node.index + 1
+            child.predicted = False
+            child = _generate_node(model, K, child, max_length)
+            children.append(child)
+        node.children = children
+
+    return node
+
+
+def _generate_node_nucleus(model, alpha, node, max_length):
+    """ Génération avec du nucleus sampling pour un noeud Beam Search """
+
+    # on arrête si le noeud a un code de fin de phrase ou si l'index max est atteint
+    if node.predicted or node.code == EOS_IX or node.index == max_length:
+        node.predicted = True
+        return node
+
+    # on prédit et on retient les meilleures prédictions
+    prediction = torch.tensor([node.code])
+    with torch.no_grad():
+        h = torch.zeros(1, model.hidden_dim)
+        h, output = model(prediction, h)
+
+        sorted_logits, sorted_indices = torch.sort(output, descending=True, dim=-1)
+        cum_probs = torch.cumsum(sorted_logits, dim=-1)
+
+        idx = cum_probs <= alpha # indices à conserver
+        idx[:, 0] = 1 # au moins 1 indice
+
+        log_probs = torch.masked_select(output, idx)
+        codes = torch.masked_select(torch.argsort(sorted_indices, dim=-1), idx)
+
+        children = []
+        for code, prob in zip(codes, log_probs):
+            child = BeamTree(code, prob)
+            child.index = node.index + 1
+            child.predicted = False
+            child = _generate_node_nucleus(model, alpha, child, max_length)
+            children.append(child)
+        node.children = children
+
+    return node
+
+
+def generate_beam(model, K, start='', max_length=200, nucleus=False, alpha=.95):
+    """ Génération avec du Beam Search """
+
+    root = [BeamTree(lettre2id[start], 0)]
+    if not nucleus:
+        root = _generate_node(model, K, root, max_length)
+    else:
+        root = _generate_node_nucleus(model, alpha, root, max_length)
+
+    # génération la plus vraisemblable
+    root = find_best(root)
+
+    # codes de la meilleure génération
+    codes = []
+    root = root.best_child # commencer après start
+    while root is not None:
+        codes.append(root.code)
+        root = root.best_child
+
+    # décodage
+    text = code2string(codes)
+    return text
+
